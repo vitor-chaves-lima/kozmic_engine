e3ad4cb920fd640503633d1b0909d4f49949953b
Vitor Chaves <vitorclcoelho@protonmail.com>
2024-11-14 13:18:04 -0300

    add kozmic platform namespace

--- sandbox/src/main.cc
+++ sandbox/src/main.cc
@@ -2,7 +2,7 @@
 
 int main() {
 	constexpr auto window_data = WindowData{L"Kozmic Sandbox", 1280, 720};
-	const auto win32_window = new Win32Window(window_data);
+	const auto win32_window = new kozmic::platform::win32::Window(window_data);
 	win32_window->Show();
 
 	while (win32_window->IsOpen()) {
